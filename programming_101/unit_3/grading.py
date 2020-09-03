420db097b52438f0e5af0e94a326bd8faadfe045
jpchato <jpchato@gmail.com>
2020-09-02 19:13:39 -0700

    lab 6 and 7 complete

--- programming_101/unit_3/grading.py
+++ programming_101/unit_3/grading.py
@@ -1,15 +1,31 @@
 grade = input('Enter a number between 0-100 that represents a letter grade: ')
 
 def test_grade(grade):
-    if (grade > '90'):
+    if (grade >= '98'):
+        print('A+')
+    elif (grade >= '92'):
         print('A')
-    elif (grade>'80'):
+    elif (grade >= '90'):
+        print('A-')
+    elif (grade >= '88'):
+        print('B+')
+    elif (grade >= '82'):
         print('B')
-    elif (grade>'70'):
+    elif (grade >= '80'):
+        print('B-')
+    elif (grade >= '78'):
+        print('C+')
+    elif (grade>'72'):
         print('C')
-    elif (grade>'60'):
+    elif (grade >= '70'):
+        print('C-')
+    elif (grade >= '68'):
+        print('D+')
+    elif (grade >= '62'):
         print('D')
-    elif (grade<'60'):
+    elif (grade >= '60'):
+        print('D-')
+    elif (grade <= '59'):
         print('F')
 
 if __name__ == "__main__":
